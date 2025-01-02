9d7287eb79249a43861e9f9d271c2424cf12d45e
Marco CARPENTIER <marcarp@protonmail.com>
2025-01-02 20:15:46 +0100

    Update signup route (user data)

--- routes/defaults.js
+++ routes/defaults.js
@@ -8,16 +8,41 @@ router.get('/', async function(req,res){
     res.render('default',{test:demo});
 });
 
-router.get('/login', async function(req,res){
+router.get('/login', function(req,res){
     res.render('login');
 });
 
-router.get('/signup', async function(req,res){
+router.get('/signup', function(req,res){
     res.render('signup');
 });
 
 router.post('/signup', async function(req,res){
-    console.log(req.body);
+    const userData = req.body;
+
+    const user = {
+        mail: userData.mail,
+        confirmMail: userData['confirm-mail'],
+        password: userData.password
+    }
+
+    const existingMail = await db.getDb().collection('users').findOne({mail: user.mail});
+
+    if(existingMail) {
+        console.log('Mail existant');
+        return res.render('signup');
+    }
+
+    if(user.mail !== user.confirmMail ||
+        user.mail.trim() < 6 ||
+        ! user.mail.includes('@') ||
+        user.password.trim() < 4
+    ) {
+        console.log("err de saisie de donnée");
+        return res.render('signup');
+    }
+
+    const result = await db.getDb().collection('users').insertOne(user);
+
     res.redirect('/');
 });
 
